96485d1c6ffa281ac1bb0f8ab5e1218415f9ba22
Souraprabha Ganguly <souraprabhaganguly26@gmail.com>
2021-07-16 19:05:51 +0530

    installed nodemon and configured dotenv

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 const app = express();
+require("dotenv/config");
 
-var http = require('http'),
-    fs = require('fs');
+const PORT = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
- 
 
-app.get("/", (req, res)=>{
-    res.render("index"); 
-});  
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get("/event", (req, res)=>{
-    res.render("event"); 
-}); 
+app.get("/event", (req, res) => {
+  res.render("event");
+});
 
-  
-app.get("/union_members", (req, res)=>{
-    res.render("union_members"); 
-}); 
+app.get("/union_members", (req, res) => {
+  res.render("union_members");
+});
 
-app.get("/gallery", (req, res)=>{
-    res.render("gallery"); 
-}); 
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
 
-app.get("/student_id", (req, res)=>{
-    res.render("student_id"); 
-}); 
+app.get("/student_id", (req, res) => {
+  res.render("student_id");
+});
 
-app.get("/gcelt", (req, res)=>{
-    res.render("gcelt"); 
-}); 
+app.get("/gcelt", (req, res) => {
+  res.render("gcelt");
+});
 
-app.get("/rc-gcelt", (req, res)=>{
-    res.render("rc-gcelt"); 
-}); 
+app.get("/rc-gcelt", (req, res) => {
+  res.render("rc-gcelt");
+});
 
-app.get("/contact", (req, res)=>{
-    res.render("contact"); 
-}); 
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
-app.listen(3005, ()=>{
-    console.log("runnnnninnngggg");
+app.listen(PORT, () => {
+  console.log("runnnnninnngggg");
 });
