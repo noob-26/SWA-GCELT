7696d4ddd24526812877f58c9118cbf596750506
PRIME_SAPTARSHI <chatterjeeu7@gmail.com>
2023-06-29 19:36:15 +0530

    uncommented the mongoose connection

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv/config");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+
 const PORT = process.env.PORT || 3000;
 
 app.use(
@@ -21,13 +22,13 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 //Connecting DB
-// mongoose.connect(
-//   process.env.MONGO_URI,
-//   {useNewUrlParser: true, useUnifiedTopology: true},
-//   () => {
-//     console.log("DB Connected!!");
-//   }
-// );
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true, useUnifiedTopology: true},
+  () => {
+    console.log("DB Connected!!");
+  }
+);
 
 //Routes
 app.use("/", require("./routes/route"));
